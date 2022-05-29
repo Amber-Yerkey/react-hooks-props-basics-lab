eb994ff1f2a1e4382fe3fce79ef8462789f1f9f2
Amber Frantz <amber.k.frantz@mgail.com>
2022-05-28 23:23:40 -0400

    Still working

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
+      {/* add if statement here?? */}
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
